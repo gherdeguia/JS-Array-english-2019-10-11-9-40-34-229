cacabfea940fc44352dcbf115b51c1862c7fb748
GERARLD DE GUIA (DEV-ISD-OOCLL/MNL) <DEGUIGE@OOCL.COM>
2021-07-23 11:58:03 +0800

    'JS_Array_Exercise'

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
+console.log("=======================================================================");
 // The writer determines whether the following variables are of type array.
 var a = '[a, b, c, d]';
 var b = [1, 2, 3, 4];
 //TODO
 
+console.log(typeof(a)+' & isArray '+ Array.isArray(a));
+console.log(typeof(b)+' & isArray '+ Array.isArray(b));
+console.log("=======================================================================");
 
 // Write a program that multiplies each entry in the following array by 2。
 var a = [1, 2, 3, 4, 5];
 // TODO should output [2,4,6,8,10]
+var multipliedItems = new Array();
+a.forEach(function(item, index, array) {
+    var multItem = item * 2;
+    multipliedItems.push(multItem);
+    //console.log(item, multItem);
+  });
+console.log(a.join(' '));
+console.log(multipliedItems);
+console.log(multipliedItems.join(' '));
 
+console.log("=======================================================================");
 
 // Write the program, according to the following requirements output results.
 var colors = ["Red", "Green", "White", "Black"];
@@ -15,12 +29,37 @@ var colors = ["Red", "Green", "White", "Black"];
 // case 2 output: 'Red+Green+White+Black'
 // case 3 output: 'Red,Green,White,Black'
 
+console.log(colors.join('+'));
+console.log(colors.join(','));
 
+console.log("=======================================================================");
 // Write a program to sort the Numbers in the following array from largest to smallest.
 var a = [5, 1, 8, 10, 4];
 //TODO should output: [10,8,5,4,1]
+a.sort(function(a, b){ return b-a; });
+console.log(a.join(' '));
 
-
+console.log("=======================================================================");
 // Program to find the most frequent element in the following array.
 var a = [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
 //TODO should output: 'a'
+var instanceCount = 0;
+var freqItem;
+var freqItemInstance = 0;
+
+a.forEach(function(item, index, array) {
+    var currItem = item;
+    for(var x = 0; x < a.length; x++){
+        if(currItem == a[x]){
+            instanceCount++;
+        }
+    }
+    if(freqItemInstance < instanceCount){
+        freqItem = currItem;
+        freqItemInstance = instanceCount;
+    }
+    //console.log(item+' '+instanceCount);
+    instanceCount = 0;
+  });
+
+console.log('Most frequent item is: '+freqItem+' with '+freqItemInstance+' of instances.');
